70dc81615c477d6f46252bca50624249935e04c5
N00rAhmed <noor.ahmed232003@outlook.com>
2023-03-22 10:09:43 +0000

    Add project files.

---
+++ Inventory/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Inventory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Create_account = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Create_account
+            // 
+            this.Create_account.Location = new System.Drawing.Point(324, 123);
+            this.Create_account.Name = "Create_account";
+            this.Create_account.Size = new System.Drawing.Size(108, 42);
+            this.Create_account.TabIndex = 0;
+            this.Create_account.Text = "create account";
+            this.Create_account.UseVisualStyleBackColor = true;
+            this.Create_account.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(324, 198);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(108, 42);
+            this.Login.TabIndex = 1;
+            this.Login.Text = "login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.Create_account);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Create_account;
+        private System.Windows.Forms.Button Login;
+    }
+}
+
---
+++ Inventory/Form3.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Inventory
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.createAccountBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(378, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Create Account";
+            // 
+            // createAccountBtn
+            // 
+            this.createAccountBtn.Location = new System.Drawing.Point(371, 305);
+            this.createAccountBtn.Name = "createAccountBtn";
+            this.createAccountBtn.Size = new System.Drawing.Size(98, 52);
+            this.createAccountBtn.TabIndex = 1;
+            this.createAccountBtn.Text = "create account";
+            this.createAccountBtn.UseVisualStyleBackColor = true;
+            this.createAccountBtn.Click += new System.EventHandler(this.createAccountBtn_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.createAccountBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button createAccountBtn;
+    }
+}
