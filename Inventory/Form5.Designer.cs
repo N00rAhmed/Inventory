bb33055446b26c72290b03a5ea337d7202877fc3
sanan862 <c2012505@hallam.shu.ac.uk>
2023-03-29 10:18:05 +0100

    Form2 page

--- Inventory/Form5.Designer.cs
+++ Inventory/Form5.Designer.cs
@@ -60,7 +60,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(314, 20);
+            this.label1.Location = new System.Drawing.Point(326, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(130, 13);
             this.label1.TabIndex = 0;
